e21bbe59f37e95694e23f5825b0ee02af0f4b822
ezio84 <brabus84@gmail.com>
2024-10-26 11:04:07 +0800

    Settings: Allow to quickly open Running Services from meminfo view neobuddy89: Updated for A14.
    Change-Id: I252ce85bda3daab5c5e4db097a6fd743365bc309
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -519,4 +519,6 @@ public class Settings extends SettingsActivity {
     
     /** Personalizations Activities **/
     public static class PersonalizationsActivity extends SettingsActivity {}
+
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -46,6 +46,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appcompat.UserAspectRatioDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
@@ -407,7 +408,8 @@ public class SettingsGateway {
             HearingDevicePairingFragment.class.getName(),
             ZenModesListFragment.class.getName(),
             ZenModeFragment.class.getName(),
-            PersonalizationsFragment.class.getName()
+            PersonalizationsFragment.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -454,6 +456,7 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
-            Settings.PersonalizationsActivity.class.getName()
+            Settings.PersonalizationsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }
