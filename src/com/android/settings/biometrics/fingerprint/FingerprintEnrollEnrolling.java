5b8b4f651ddd263a66b495e0cf0df6cc591ce8c8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-18 07:23:25 +0800

    Revert "FingerPrintEnrollEnrolling: Avoid setting footer background"
    This reverts commit 4e757fe23f8b791ddcc0755ec3fc4cd4cd688046.

--- src/com/android/settings/biometrics/fingerprint/FingerprintEnrollEnrolling.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintEnrollEnrolling.java
@@ -32,7 +32,6 @@ import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Animatable2;
@@ -298,17 +297,12 @@ public class FingerprintEnrollEnrolling extends BiometricsEnrollEnrolling {
                         .setTheme(com.google.android.setupdesign.R.style.SudGlifButton_Secondary)
                         .build()
         );
-        final android.widget.LinearLayout buttonContainer = mFooterBarMixin != null
-                ? mFooterBarMixin.getButtonContainer()
-                : null;
-        if (buttonContainer != null) {
-            buttonContainer.setBackgroundColor(Color.TRANSPARENT);
-        }
 
         // If it's udfps, set the background color only for secondary button if necessary.
         if (mCanAssumeUdfps) {
             mShouldSetFooterBarBackground = false;
-            ((UdfpsEnrollEnrollingView) getLayout()).setSecondaryButtonBackground(Color.TRANSPARENT, true);
+            ((UdfpsEnrollEnrollingView) getLayout()).setSecondaryButtonBackground(
+                    getBackgroundColor());
         }
 
         final LayerDrawable fingerprintDrawable = mProgressBar != null
